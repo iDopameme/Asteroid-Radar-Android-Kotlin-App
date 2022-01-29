1a627b8fd8f12e569e0f8d9023b316c992b1564c
Richard Walker <Dopameme@outlook.com>
2022-01-29 17:58:27 -0500

    Removed API KEY from Constants.kt and indicated to utilize Constants.kt for inputting each devs own individual API KEY

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # NASA Asteroid Android App using NASA Provided API
 
-
+Please utilize the Constant.kt object file to input your respective NASA API Key to ensure 
+that the project functions as intended. A Constant "API_KEY" is already defined.  
--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "ujUZffPt0DhgIASXqhhxRXUZfDdbyHfXSkA88m0P"
+    // Enter in your NASA API key here
+    const val API_KEY = ""
 }
